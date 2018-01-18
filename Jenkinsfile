08334787139ffab6168b0f274a9dc4fec05e09ab
badrish mishra <badrishmishra@gmail.com>
2018-01-18 13:13:05 +0530

    2

---
+++ Jenkinsfile
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+   // tools { // <1>
+       // maven 'maven 3.2.5' // <2>
+        //jdk 'jdk1.8.0_151' // <3>
+   // }
+    stages {
+        stage ('Initialize') {
+            steps {
+                println("Initialize")
+				bat '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
+
+        stage ('Build') {
+            steps {
+               println("Build")
+			   bat "mvn clean"
+            }
+        }
+
+        stage ('test') {
+                    steps {
+                       bat "mvn install"
+                    }
+        }
+
+    }
+}
---
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -0,0 +1,98 @@
+/*
+ * @author Naveen Khunteta
+ * 
+ */
+
+package com.crm.qa.testcases;
+
+import java.io.IOException;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+public class ContactsPageTest extends TestBase{
+
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	ContactsPage contactsPage;
+	
+	String sheetName = "contacts";
+	
+	   
+	public ContactsPageTest(){
+			super();
+			
+	}
+	
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		initialization();
+		testUtil = new TestUtil();
+		contactsPage = new ContactsPage();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		testUtil.switchToFrame();
+		contactsPage = homePage.clickOnContactsLink();
+	}
+	
+	@Test(priority=1)
+	public void verifyContactsPageLabel(){
+		Assert.assertTrue(contactsPage.verifyContactsLabel(), "contacts label is missing on the page");
+	}
+	
+	@Test(priority=2)
+	public void selectSingleContactsTest(){
+		contactsPage.selectContactsByName("test2 test2");
+	}
+	
+	@Test(priority=3)
+	public void selectMultipleContactsTest(){
+		contactsPage.selectContactsByName("test2 test2");
+		contactsPage.selectContactsByName("ui uiii");
+
+	}
+	
+	@DataProvider
+	public Object[][] getCRMTestData(){
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	
+	@Test(priority=4, dataProvider="getCRMTestData")
+	public void validateCreateNewContact(String title, String firstName, String lastName, String company){
+		homePage.clickOnNewContactLink();
+		//contactsPage.createNewContact("Mr.", "Tom", "Peter", "Google");
+		contactsPage.createNewContact(title, firstName, lastName, company);
+		
+	}
+	
+	
+
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+}
